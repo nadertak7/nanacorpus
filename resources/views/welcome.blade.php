684b5c33f8ad5e4345db1abec383b3e45b458f20
nadertak7 <nadertak7@gmail.com>
2022-12-10 23:23:32 +0000

    Added navbar and background

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,39 @@
 <!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
 
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
+<html>
+
+<head>
+<title>nanacorpus</title>
+<link rel="stylesheet" href="app.css">
+</head>
+
+<body>
+  <div class="hero">
+
+      <video autoplay loop muted plays-inline class=background-video> 
+        <source src="{{ URL('videos/NATURE.mp4') }}" type = "video/mp4">
+      </video> 
+
+
+      <nav> 
+        <a href="/"><img src="{{ URL('images/logo.jpg') }}" alt="logo" class="logo"></a>
+        <ul>
+            <li> <a href='#'> Home </a> </li> 
+            <li> <a href='#'> Nav1 </a> </li> 
+            <li> <a href='#'> Nav2 </a> </li> 
+            <li> <a href='#'> Nav3 </a> </li> 
+        </ul> 
+      </nav> 
+      <div class="content"> 
+        <h1>nanacorpus</h1>
+        <h2>web corpus toolkit<h2> 
+        <br> 
+        
+        <a href="#">Click here to get started</a>
+      </div>   
+  </div> 
+
+
 
 </body>
 </html>
