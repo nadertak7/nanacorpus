c3dc97e8982e27d4e52db04163ad116ef5b443c1
nadertak7 <nadertak7@gmail.com>
2022-12-11 18:34:21 +0000

    Added vue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,44 +2,54 @@
 
 <html lang="en">
 
-<head>
+  <head>
 
-  <title>nanacorpus</title>
+    <title>nanacorpus</title>
 
-  <link rel="stylesheet" href="app.css">
+    <link rel="stylesheet" href="app.css">
 
-</head>
+  </head>
 
-<body>
- 
-  <div class="mainscreen">
+  <body>
+    <div id="app"> 
 
-      <video autoplay loop muted plays-inline class=background-video> 
-        <source src="{{ URL('videos/NATURE.mp4') }}" type = "video/mp4">
-      </video> 
+      <div class="mainscreen">
+
+          <video autoplay loop muted plays-inline class=background-video> 
+            <source src="{{ URL('videos/NATURE.mp4') }}" type = "video/mp4">
+          </video> 
 
 
-      <nav> 
-        <a href="/"><img src="{{ URL('images/home3.jpg') }}" alt="logo" class="logo"></a>
-        <ul>
-            <li> <a href='#'> Tools </a> </li> 
-            <li> <a href='#'> Citation </a> </li> 
-            <li> <a href='#'> Account </a> </li> 
-            <li> <a href='https://github.com/nadertak7' target="_blank"> GitHub </a> </li> 
-        </ul> 
-      </nav> 
-      
-      <div class="content"> 
-        <h1>nanacorpus</h1>
-        <h2>web corpus toolkit<h2> 
-        <br> 
-        
-        <a href="#">Click here to get started</a>
-      </div>   
+          <nav> 
+            <a href="/"><img src="{{ URL('images/home3.jpg') }}" alt="logo" class="logo"></a>
+            <ul>
+                <li> <a href='#'> Tools </a> </li> 
+                <li> <a href='#'> Citation </a> </li> 
+                <li> <a href='#'> Account </a> </li> 
+                <li> <a href='https://github.com/nadertak7' target="_blank"> GitHub </a> </li> 
+            </ul> 
+          </nav> 
+          
+          <div class="content"> 
+            <h1>nanacorpus</h1>
+            <h2>web corpus toolkit<h2> 
+            <br> 
+            
+            <a href="#">Click here to get started</a>
+          </div>  
+      </div> 
+    
+<!--    <script src="https://unpkg.com/vue@next"></script>
+    <script> 
+    let app = Vue.createApp({})
+    app.mount('#app')
+    </script>
+-->  
   
-
-</body>
-
-
   </div> 
+    
+  </body>
+
+
+
 </html>
